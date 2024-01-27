b241f8267de3223ef5a568b518522cd5dfe340a9
ckantas <cka@create.aau.dk>
2024-01-27 10:59:01 +0100

    l

--- downsample_ply.py
+++ downsample_ply.py
@@ -1,55 +1,127 @@
 import open3d as o3d
 import sys
 
+def_voxel_size = 2.5
+def_k_points = 5
+
 def voxel_downsampling(point_cloud, voxel_size):
+    print(f'Downsampling point cloud using voxel downsampling (voxel_size: {voxel_size})')
     voxel_down_pcd = point_cloud.voxel_down_sample(voxel_size=voxel_size)
     return voxel_down_pcd
 
 def uniform_downsampling(point_cloud, k_points):
+    print(f'Downsampling point cloud using uniform downsampling (every_k_points = {k_points})')
     uniform_down_pcd = point_cloud.uniform_down_sample(every_k_points=k_points)
     return uniform_down_pcd
 
-point_cloud_path = '/home/chris/Code/PointClouds/data/point_cloud_files/VentilationGrate_2.ply'
+def downsample_pcd(pcd, type):
+    voxel_size = 0
+    k_points = 0
+    redo = True
+    orig_num_points = len(pcd.points)
 
-pcd = o3d.io.read_point_cloud(point_cloud_path)
-orig_num_points = len(pcd.points)
-
-voxel_size = 2.5
-k_points = 5
-
-type = None
-while type == None:
-    type = input('Choose voxel downsampling, uniform downsampling or exit code [v/u/e]:')
     if type == 'v':
-        print(f'Downsampling point cloud using voxel downsampling (voxel_size: {voxel_size})')
-        ds_pcd = voxel_downsampling(point_cloud=pcd, voxel_size=voxel_size)
+        while redo == True:
+            voxel_size = input(f'Choose voxel_size (default = {def_voxel_size}, allowed range 0.5-5):')
+            flag = True
+            while flag == True:
+                try: 
+                    _ = float(voxel_size)
+                except:
+                    print('Value entered not a number')
+                    flag = False
+                    continue
+                if float(voxel_size) >= 0.5 and float(voxel_size) <= 5:
+                    ds_pcd = voxel_downsampling(point_cloud=pcd, voxel_size=float(voxel_size))
+                    down_num_points = len(ds_pcd.points)
+                    print(f'Done!\n\nOriginal number of points: {orig_num_points}\nNumber of points after downsampling: {down_num_points}\nVisualizing point cloud...')
+                    o3d.visualization.draw_geometries([ds_pcd])
+                    redo_flag = True
+                    while redo_flag == True:
+                        redo_input = input('Choose different voxel_size? [y/n]:')
+                        if redo_input == 'n':
+                            redo = False
+                            redo_flag = False
+                        elif redo_input == 'y':
+                            redo = True 
+                            redo_flag = False
+                        else:
+                            print('Invalid input. Try again')
+                        flag = False
+                else:   
+                    again = input('Invalid input. Try again? [y/n]:')
+                    if again == 'y':
+                        flag = False
+                    else:
+                        print('Exiting program')
+                        sys.exit()
     elif type == 'u':
-        print(f'Downsampling point cloud using uniform downsampling (every_k_points = {k_points})')
-        ds_pcd = uniform_downsampling(point_cloud=pcd, k_points=k_points)
+        while redo == True:
+            k_points = input(f'Choose every_k_points (default = {def_k_points}, allowed range 2-500):')
+            flag = True
+            while flag == True:
+                try: 
+                    _ = float(k_points)
+                except:
+                    print('Value entered not a number')
+                    flag = False
+                    continue
+                if float(k_points) >= 2 and float(k_points) <= 500:
+                    ds_pcd = uniform_downsampling(point_cloud=pcd, k_points=int(k_points))
+                    down_num_points = len(ds_pcd.points)
+                    print(f'Done!\n\nOriginal number of points: {orig_num_points}\nNumber of points after downsampling: {down_num_points}\nVisualizing point cloud...')
+                    o3d.visualization.draw_geometries([ds_pcd])
+                    redo_flag = True
+                    while redo_flag == True:
+                        redo_input = input('Choose different value for every_k_points? [y/n]:')
+                        if redo_input == 'n':
+                            redo = False
+                            redo_flag = False
+                        elif redo_input == 'y':
+                            redo = True 
+                            redo_flag = False
+                        else:
+                            print('Invalid input. Try again')
+                        flag = False
+                else:   
+                    again = input('Invalid input. Try again? [y/n]:')
+                    if again == 'y':
+                        flag = False
+                    else:
+                        print('Exiting program')
+                        sys.exit()
     elif type == 'e':
         print('Exiting program')
         sys.exit()
     else:
         print('Invalid input. Try again')
         type = None
+    return ds_pcd, voxel_size, k_points, type
 
-down_num_points = len(ds_pcd.points)
-print(f'Done!\n Original number of points: {orig_num_points}\n Number of points after downsampling: {down_num_points}\n Visualizing point cloud...')
 
-o3d.visualization.draw_geometries([ds_pcd])
+
+
+point_cloud_path = '/home/chris/Code/PointClouds/data/point_cloud_files/VentilationGrate_2.ply'
+pcd = o3d.io.read_point_cloud(point_cloud_path)
+
+type = None
+while type == None:
+    type = input('Choose between voxel downsampling, uniform downsampling or exit code [v/u/e]:')
+    ds_pcd, voxel_size, k_points, type = downsample_pcd(pcd, type)
 
 save = None
 while save == None:
-    save= input('Save downsampled point cloud? [y/n]:')
+    save= input('\nSave downsampled point cloud? [y/n]:')
     if save == 'y':
         if type == 'v':
-            output_path = point_cloud_path.split('.')[0]+'_voxelds.ply'
-            print(print(f'Saving in {output_path}'))
+            output_path = point_cloud_path.split('.')[0]+'_voxeldown' + voxel_size + '.ply'
+            print(f'Saving in {output_path}')
             o3d.io.write_point_cloud(output_path, ds_pcd)
         elif type == 'u':
-            output_path = point_cloud_path.split('.')[0]+'_uniformds.ply'
-            print(print(f'Saving in {output_path}'))
+            output_path = point_cloud_path.split('.')[0]+'_unidown' + k_points + '.ply'
+            print(f'Saving in {output_path}')
             o3d.io.write_point_cloud(output_path, ds_pcd)
+        sys.exit()
     elif save == 'n':
         print('Exiting program')
         sys.exit()
